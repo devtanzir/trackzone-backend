3e954b0f1e313a5691e5ac298ad610b8eacca459
tanzir ibne ali <tanziribneali@gmail.com>
2024-05-27 16:57:18 +0600

    cors and dotenv added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import Express from "express";
 import { connectDb } from "./db.js";
 import routes from "./routes/index.js";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = Express();
 app.use(Express.json());
+app.use(cors());
 app.use(routes);
 
 app.get("/", (_req, res) => {
@@ -28,11 +33,11 @@ app.use((err, _req, res, _next) => {
   res.status(status).json({ message });
 });
 
+const PORT = process.env.PORT || 8000 || 5001;
+
 connectDb("mongodb://localhost:27017/track-zone").then(() => {
   console.log("database connected");
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
-
-const PORT = 5000;
